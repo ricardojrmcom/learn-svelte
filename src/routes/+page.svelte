6707f88cae2c7957adff0f27203e753e35aeee44
ricardojrmcom <liber@ricardojrm.com>
2022-08-27 06:02:04 +0100

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import Nav from '$lib/components/Nav/index.svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Home" />
+</svelte:head>
+
+<section>
+	<div>
+		<Nav />
+	</div>
+	<h1>Home</h1>
+</section>
+
+<style>
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import Nav from '$lib/components/Nav/index.svelte';
+</script>
+
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About" />
+</svelte:head>
+
+<section>
+	<div>
+		<Nav />
+	</div>
+	<h1>About</h1>
+</section>
+
+<style>
+</style>
